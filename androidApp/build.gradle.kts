5d5e3d1482720a2046d6652acc4641be707add4a
riko <riko.lunch@gmail.com>
2026-08-17 18:17:37 +0900

    PDFを開いてマーカーを引いて保存

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,6 +15,10 @@ dependencies {
 
     implementation(libs.androidx.activity.compose)
 
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.ui)
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
 }
