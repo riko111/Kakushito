49caf114dff46a6bbc468ca426f932b3f5475354
riko <riko.lunch@gmail.com>
2026-08-21 10:11:10 +0900

    パレットアイコンと問題修正

--- androidApp/src/main/kotlin/com/isoffice/kakushito/KakushitoApp.kt
+++ androidApp/src/main/kotlin/com/isoffice/kakushito/KakushitoApp.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.detectTapGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
