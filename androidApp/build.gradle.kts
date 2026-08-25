ed5d16c3f4a7f836a94531da0a1e6cb4a28bf393
riko <riko.lunch@gmail.com>
2026-08-26 07:56:11 +0900

    初期表示のexception追記

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.isoffice.kakushito"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "1.0.2"
+        versionCode = 8
+        versionName = "1.0.4"
     }
     packaging {
         resources {
