27410afedfcece9eed5381ecd86959ea06ab9125
riko <riko.lunch@gmail.com>
2026-08-27 08:37:59 +0900

    Add JS web entry point with Firebase login callback

---
+++ webApp/src/jsMain/kotlin/com/isoffice/kakushito/main.kt
@@ -0,0 +1,15 @@
+package com.isoffice.kakushito
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App(
+            onGoogleLogin = {
+                startFirebaseLogin()
+            }
+        )
+    }
+}
