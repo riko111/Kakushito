b8381b662c82d0ba9977f3e8e47b462a49e8365e
riko <riko.lunch@gmail.com>
2026-08-29 19:36:12 +0900

    Enable Ktor CORS dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.cors)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.mysql.connector)
     implementation(libs.hikari)
